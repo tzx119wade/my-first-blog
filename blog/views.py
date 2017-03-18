5cd314691ca742431cb405c0d47aa5fef3f3c1f0
tangzhenxing <439930322@qq.com>
2017-03-18 16:45:17 +0800

    评论功能完成，还差评论删除功能

--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render,get_object_or_404,redirect
 from .models import Post
-from .forms import PostForm
+from .forms import PostForm,CommentForm
 from django.utils import timezone
 from django.contrib.auth.decorators import login_required
 
@@ -79,3 +79,20 @@ def post_remove(request,pk):
     post.delete()
 
     return redirect('post_list')
+
+# 添加评论
+def add_comment_to_post(request,pk):
+    post = get_object_or_404(Post,pk=pk)
+
+    if request.method == 'POST':
+        form = CommentForm(request.POST)
+        if form.is_valid():
+            comment = form.save(commit=False)
+            comment.post = post
+            comment.save()
+            return redirect('post_detail',pk = post.pk)
+
+    else:
+        form = CommentForm()
+    return render(request,'blog/add_comment_to_post.html', {'form':form})
+
